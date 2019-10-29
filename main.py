606bffa484e56e87fa52f545a487c97c2371d4da
C. Yerger <yerger.c.work@gmail.com>
2019-10-29 14:46:20 +0000

    roadmap & basic IO

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import splash
+import sys
+name = input("Before we begin, what's your name?\n")
+print("Hello, ", name, "!")
+print(name, ", you're really such a ")
+# insult function
+retry = input("\nWould you like to hear another one? (y/n)\n")
+# if retry == "y":
+#   insult()
+# else:
+#   print("\nGoodbye.")
+#   exit()
