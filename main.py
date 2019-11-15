fc695ce46dff86aa2917db995e0250493ea498d5
C. Yerger <yerger.c.work@gmail.com>
2019-11-15 16:11:51 +0000

    cleaning

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def insult():
     adjPrint = random.choice(adj)
     nounPrint = random.choice(noun)
     verbPrint = random.choice(verb)
-    specialPrint = ""
 
-    if random.random() < 50:
+    if random.random() < 50:  # 50% chance for special comment to be printed, otherwise it's blank
         specialPrint = random.choice(special)
-    
+    else:
+        specialPrint = ""
     print(name,", you're such ",specialPrint, adjPrint, nounPrint, verbPrint,"!")  # ugly, but it works.
 insult()
+
 retry = input("\nWould you like to hear another one? (y/n)\n")
 # if retry == "y":
 #   insult()
