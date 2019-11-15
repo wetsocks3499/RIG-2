7c309ea88de238de3e0c238fadb38d37a0a6c558
C. Yerger <yerger.c.work@gmail.com>
2019-11-15 16:03:39 +0000

    adding py module that lists insult combos; imported in main

---
+++ compo.py
@@ -0,0 +1,9 @@
+#  array of insult "blocks," seperated into three parts.
+#  ADJ => NOUN => VERB
+adj = ['a messy', 'a disheveled', 'a monkey-brained', 'a poorly-packaged pile of', 'a crunchy', 'an oblique', 'a flat-headed']
+
+special = ['Flat-Earth believing']
+
+noun = ['donkey', 'ogre', 'chicken', 'child']
+
+verb = ['licker', 'pleaser', 'imitator']
