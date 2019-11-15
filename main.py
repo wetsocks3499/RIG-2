a6f9902c6de4f003e9ce7377eceea64349ed95a9
C. Yerger <yerger.c.work@gmail.com>
2019-11-15 16:02:47 +0000

    initial code 75% complete

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+#########
 import splash
+from compo import *
 import sys
+import random
+#########
+
 name = input("Before we begin, what's your name?\n")
-print("Hello, ", name, "!")
-print(name, ", you're really such a ")
-# insult function
+print("Hello,", name,"!")
+def insult():
+    adjPrint = random.choice(adj)
+    nounPrint = random.choice(noun)
+    verbPrint = random.choice(verb)
+    specialPrint = ""
+
+    if random.random() < 50:
+        specialPrint = random.choice(special)
+    
+    print(name,", you're such ",specialPrint, adjPrint, nounPrint, verbPrint,"!")  # ugly, but it works.
+insult()
 retry = input("\nWould you like to hear another one? (y/n)\n")
 # if retry == "y":
 #   insult()
