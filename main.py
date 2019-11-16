85423b989c6c4a04d7d4a617a9534d594f3061c9
C. Yerger <yerger.c.work@gmail.com>
2019-11-16 01:47:00 +0000

    Restart function update

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def insult():
     nounPrint = random.choice(noun)
     verbPrint = random.choice(verb)
 
-    if random.random() < 50:  # 50% chance for special comment to be printed, otherwise it's blank
+    if random.random() < 15:  # 15% chance for special comment to be printed, otherwise it's blank
         specialPrint = random.choice(special)
     else:
         specialPrint = ""
-    print(name,", you're such ",specialPrint, adjPrint, nounPrint, verbPrint,"!")  # ugly, but it works.
+    print(name,", you're such "+specialPrint+adjPrint+nounPrint+verbPrint+"!")  # ugly, but it works.
 insult()
 
-retry = input("\nWould you like to hear another one? (y/n)\n")
 def restart():
+    retry = input("\nWould you like to hear another one? (y/n)\n")
     if retry == "y":
         insult()
     else:
