c93920a8426a426b8b955abf98076c8b1756b774
ydissac <yerger.c.work@gmail.com>
2019-12-08 14:19:19 -0500

    changed percentage of special from 50 to 15

--- test_main.py
+++ test_main.py
@@ -13,9 +13,9 @@ def insult():
     verbPrint = random.choice(verb)
     specialPrint = ""
 
-    if random.random() < 50:
+    if random.random() < 15:
         specialPrint = random.choice(special)
-    
+
     print(name,", you're such ",specialPrint, adjPrint, nounPrint, verbPrint,"!")  # ugly, but it works.
 insult()
 retry = input("\nWould you like to hear another one? (y/n)\n")
