7b43a4d248d2c99b90403ca277fbb658a08b5573
ydissac <yerger.c.work@gmail.com>
2019-12-08 14:18:17 -0500

    adding terms

--- compo.py
+++ compo.py
@@ -2,8 +2,8 @@
 #  ADJ => NOUN => VERB
 adj = ['a messy', 'a disheveled', 'a monkey-brained', 'a poorly-packaged pile of', 'a crunchy', 'an oblique', 'a flat-headed']
 
-special = ['Flat-Earth believing']
+special = ['Flat-Earth believing', 'neck-bearded']
 
-noun = ['donkey', 'ogre', 'chicken', 'child']
+noun = ['donkey', 'ogre', 'chicken', 'child', 'teacher']
 
-verb = ['licker', 'pleaser', 'imitator']
+verb = ['licker', 'pleaser', 'imitator', 'slapper', 'eater']
