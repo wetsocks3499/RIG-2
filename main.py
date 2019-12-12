aef18fe8f7a431152b9d723ba79f319e366d1241
C. Yerger <yerger.c.work@gmail.com>
2019-12-12 16:14:17 +0000

    stuff

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 import splash
 from compo import *
 import random
-import colorama
+from colorama import Style, Fore
 #########
-
-name = input("Before we begin, what's your name?\n")
+print(Style.RESET_ALL)
+name = input("Before we begin, what's your name?\n>>> ")
 print("\nHello, "+name+"!\n")
 def insult():
     adjPrint = random.choice(adj)
@@ -13,12 +13,13 @@ def insult():
     verbPrint = random.choice(verb)
     specialPrint = "" # storage value
 
-    if random.random() == 15: # 5% chance
+    if random.random(1, 100) == 15: # 15% chance
         specialPrint = random.choice(special)
 
-    print(name+", you're a"+specialPrint+adjPrint+nounPrint+verbPrint+"!")  # ugly, but it works.
+    print(name+", you're a"+Fore.RED+specialPrint+adjPrint+nounPrint+verbPrint+"!")  # ugly, but it works.
     specialPrint = "" # resets the variable
     retry = "y"
+    print(Style.RESET_ALL)
     retry = input("\nWould you like to hear another one? (y/n)\n")
     if retry == "y":
         retry = ""
